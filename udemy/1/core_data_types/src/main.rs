805f76d0772c9e527332cea38b4f49c943b80acd
axayjha <akshayjha@live.in>
2020-06-10 15:38:59 +0530

    update

--- udemy/1/core_data_types/src/main.rs
+++ udemy/1/core_data_types/src/main.rs
@@ -1,8 +1,12 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 
+use std::mem;
+
 fn main() {
     println!("Hello, world!");
+
+    // Integral types
 
     let a:u8 = 123;
     println!("a = {}", a);
@@ -14,5 +18,28 @@ fn main() {
     b = 1;
     println!("b = {}", b);
 
+    let mut c = 123456789;
+    println!("c = {}, size = {} bytes", c, mem::size_of_val(&c));
 
+    // u8 i8 u16 i16 u32 i32 u64 i64
+    // isize/usize
+
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!("z = {}, takes up {} bytes, {}-bit OS",
+        z, size_of_z, size_of_z*8);
+
+    // char
+    let d = 'x';
+    println!("d = {}, size = {} bytes", d, mem::size_of_val(&d));
+    // size = 4 bytes -> not ASCII, highest UNICODE size possible
+
+    // fp, double
+    let e = 2.5; // f64 by default -> double precision
+    println!("e = {}, size = {} bytes", e, mem::size_of_val(&e));
+
+    // bool
+    let g = false;
+    println!("g = {}, size = {} bytes", g, mem::size_of_val(&g));
+    
 }
--- udemy/1/core_data_types/target/debug/deps/core_data_types-03b65b83ab888570.d
+++
@@ -1,5 +0,0 @@
-/Users/akshay/Documents/rust/udemy/1/core_data_types/target/debug/deps/core_data_types-03b65b83ab888570: src/main.rs
-
-/Users/akshay/Documents/rust/udemy/1/core_data_types/target/debug/deps/core_data_types-03b65b83ab888570.d: src/main.rs
-
-src/main.rs:
