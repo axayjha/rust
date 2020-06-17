f4aa75391b2280ec70ec76aa24ea9f0be0b75b03
axayjha <akshayjha@live.in>
2020-06-17 22:10:39 +0530

    added concurrency example

--- udemy/10/concurrency/src/main.rs
+++ udemy/10/concurrency/src/main.rs
@@ -1,5 +1,28 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#[allow(unused_must_use)]
 
+use std::thread;
+use std::time;
+
+// spawning and joining threads
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
+
+    let handle = thread::spawn(|| {
+        for _ in 1..10 {
+            print!("+");
+            thread::sleep(time::Duration::from_millis(500));
+        }
+    });
+
+    for _ in 1..10 {
+        print!("-");
+        thread::sleep(time::Duration::from_millis(300));
+    }
+
+    handle.join();
+
+
 }
